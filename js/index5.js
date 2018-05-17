cb6d8f14902f3b195d0ac0bbeb1ef3a62a15a08c
merethom <merethom@users.noreply.github.com>
2018-05-16 23:39:15 -0400

    refreshing website

--- js/index5.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	var onMobile = false;
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) { onMobile = true; }
-	if( ( onMobile === false ) ) {
-		$("body").hasClass("nobg");
-	} else {
-		$("body").removeClass("nobg");
-	}
-});
